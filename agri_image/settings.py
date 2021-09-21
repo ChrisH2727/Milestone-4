ad07f67d9bc20a346eef362cc3333d169355b02b
ChrisH2727 <chris@hullbrook.net>
2021-09-21 12:38:20 +0000

    Setup for postgres db

--- agri_image/settings.py
+++ agri_image/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'agri_image.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
+if 'DATABASE_URL2' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }

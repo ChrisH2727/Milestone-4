fba635836f3e89fad94a165481247f2e390a279a
ChrisH2727 <chris@hullbrook.net>
2021-10-28 14:58:16 +0000

    Add subscription model and make available in admin

--- pricing/admin.py
+++ pricing/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Subscription
 
-# Register your models here.
+
+class Subscription(admin.SubscriptionAdmin):
+    list_display = (
+        'sub_type',
+        'sub_name',
+        'sub_display_name',
+        'sub_display_description',
+        'sub_duration',
+        'sub_images',
+        'sub_price',
+    )
+
+# Register Subscription models.
+
+admin.site.register(Subscription, SubscriptionAdmin)

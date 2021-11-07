f55beae893492569ad786dd21e01328350d264a5
ChrisH2727 <chris@hullbrook.net>
2021-11-07 13:49:35 +0000

    implement image request delete

--- management/urls.py
+++ management/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.management, name='management'),
     path('edit_request/<int:ask_id>/', views.edit_request, name='edit_request'),
-    path('delete_request/<int:request_id>', views.delete_request, name='delete_request'),
-    path('<int:ask_id>/', views.edit_request, name='edit_request'),
-    path('', views.delete_request, name='edit_request'),   
+    path('delete_request/<int:ask_id>/', views.delete_request, name='delete_request'),
+    path('', views.edit_request, name='edit_request'),
+    path('', views.delete_request, name='delete_request'),
 ]
--- management/views.py
+++ management/views.py
@@ -20,52 +20,60 @@ def edit_request(request, ask_id):
     """
     Edit the request for an image
     """
-    print("edit_request ", ask_id)
+
     if request.method == 'POST':
         response_form = ResponseForm(request.POST)
-        # save image request 
+        # save image request
         if response_form.is_valid():
-            print("form valid")
             response_form.save()
-
-            # send confirmation email
+            
             #send_confirmation_email(image_response)
 
             return redirect(management)
         else:
             messages.error(request, f'Please complete the form correctly')
-            print("form not valid")
 
     else:
         request_for_image = get_object_or_404(RequestImage, id=ask_id)
         form = ResponseForm(instance=request_for_image)
     
     template = 'management/edit_request.html'
+
     context = {
         'form': form,
+        'ask_id': ask_id
         }
 
     return render(request, template, context)
     
 
 
-def delete_request(request, request_id):
+def delete_request(request, ask_id):
     """
     Delete the request for an image
     """
-    request_for_image = get_object_or_404(RequestImage, id=request_id)
-    form = ResponseForm(instance=request_for_image)
 
-    # response_form = ResponseForm()
     if request.method == 'POST':
-        form = RequestForm(request.POST or None)
-        print("post")
-        return redirect(management)
+        response_form = ResponseForm(request.POST)
+        # delete image request 
+        if response_form.is_valid():
+            #response_form.save()
+            RequestImage.objects.filter(id=ask_id).delete()
+            #send_confirmation_email(image_response)
+
+            return redirect(management)
+        else:
+            messages.error(request, f'The image request has not been deleted.')
+    
     else:
-        template = 'management/delete_request.html'
-        print("get")
-        context = {
-            'form': form,
+        request_for_image = get_object_or_404(RequestImage, id=ask_id)
+        form = ResponseForm(instance=request_for_image)
+        
+    template = 'management/delete_request.html'
+
+    context = {
+        'form': form,
+        'ask_id': ask_id,
         }
 
     return render(request, template, context)

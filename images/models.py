a0c18e72b0d806d4a37f79740041cd460aa73569
ChrisH2727 <chris@hullbrook.net>
2021-11-25 11:28:48 +0000

    Fix defect in image model to enable e display

--- images/models.py
+++ images/models.py
@@ -27,8 +27,7 @@ class Image(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2, default=1.00)
     views = models.PositiveIntegerField(null=True, blank=True, default=1)
     downloads = models.PositiveIntegerField(null=True, blank=True, default=1)
-    image = models.CharField(max_length=254, null=True, blank=True)
-
+    image = models.ImageField(null=True, blank=True)
     def __str__(self):
         return self.name
 

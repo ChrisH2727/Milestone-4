2029f5722091def73a8ebbaad893ca7fa16e5ec9
ChrisH2727 <chris@hullbrook.net>
2021-11-26 08:28:50 +0000

    Setup for Heroku deployment

--- agri_image/settings.py
+++ agri_image/settings.py
@@ -127,7 +127,7 @@ WSGI_APPLICATION = 'agri_image.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
+if 'DATABASE_URL2' in os.environ:
     print("local")
     DATABASES = {
         'default': {
@@ -171,7 +171,6 @@ USE_TZ = True
 # Constants
 SALES_TAX_RATE = 20
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -183,7 +182,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # ............................Need to set USE_AWS......
 
-if 'USE_AWSs' in os.environ:
+if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
